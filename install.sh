0bfdc02064f6d42e09024f1f770e16e5ae2578fb
Tushar Chaturvedi <chaturvedi.t@husky.neu.edu>
2017-05-08 13:11:46 -0700

    added gitconfig

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Installing applications..."
+sudo apt-get -y install zsh openbox openbox-menu firefox terminator wicd wicd-curses tint2 nitrogen xcompmgr neovim curl
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cd $HOME
+
+echo "Linking zshrc"
+ln -s $HOME/.dotfiles/zsh_config/zshrc .zshrc
+
+cd $HOME/.config
+
+echo "Linking application configurations"
+ln -s $HOME/.dotfiles/terminator terminator
+ln -s $HOME/.dotfiles/nvim nvim
+ln -s $HOME/.dotfiles/openbox openbox
---
+++ openbox/autostart.sh
@@ -0,0 +1,11 @@
+# start panel
+tint2 &
+
+# restore wallpaper
+nitrogen --restore &
+
+# enable animations
+xcompmgr &
+
+# start docky
+docky &
