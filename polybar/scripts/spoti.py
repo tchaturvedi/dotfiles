85d935d18febcb3d302dfacb2060a7746e89611e
Tushar Chaturvedi <chaturvedi.t@husky.neu.edu>
2017-08-27 21:46:21 -0700

    update

--- polybar/scripts/spoti.py
+++ polybar/scripts/spoti.py
@@ -6,11 +6,12 @@ cmd = os.popen('playerctl -p spotify status').read()
 cmd = cmd.split('\n')
 cmd = cmd[0]
 
-meta_artist = os.popen('playerctl -p spotify metadata xesam:artist').read()
-meta_title = os.popen('playerctl -p spotify metadata xesam:title').read()
+if not cmd == 'Not available':
+    meta_artist = os.popen('playerctl -p spotify metadata xesam:artist').read()
+    meta_title = os.popen('playerctl -p spotify metadata xesam:title').read()
 
-if cmd == "Playing":
-    print(meta_title + ' - ' + meta_artist)
-elif cmd == 'Paused':
-    print(meta_title + ' - ' + meta_artist)
+    if cmd == "Playing":
+        print(meta_title + ' - ' + meta_artist)
+    elif cmd == 'Paused':
+        print(meta_title + ' - ' + meta_artist)
 
